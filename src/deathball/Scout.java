3e3dc3e2fcbebf124764366758e33926e659a6a1
Samuel Kim <ubuntor2000@gmail.com>
2016-01-12 12:40:52 -0500

    scout efficiency

--- src/deathball/Scout.java
+++ src/deathball/Scout.java
@@ -7,8 +7,8 @@ import battlecode.common.*;
  */
 public class Scout {
     public static void run(RobotController rc) {
-		MapLocation home = rc.getLocation();
-		Direction dirToMove = Direction.EAST;
+        MapLocation home = rc.getLocation();
+        Direction dirToMove = Direction.EAST;
         int spiralCount = 1;
         int stepCount = 7;
         try {
@@ -21,7 +21,7 @@ public class Scout {
             try {
                 RobotInfo[] enemies = rc.senseHostileRobots(rc.getLocation(), 53);
                 for (int i = 0; i < enemies.length; i++) {
-                    if(rc.getLocation().distanceSquaredTo(enemies[i].location) <= 26){
+                    if (rc.getLocation().distanceSquaredTo(enemies[i].location) <= 26) {
                         if (rc.isCoreReady()) {
                             // Check the rubble in that direction
                             if (rc.canMove(enemies[i].location.directionTo(rc.getLocation()))) {
@@ -30,17 +30,15 @@ public class Scout {
                             }
                         }
                     }
-                    if(enemies[i].team == rc.getTeam().opponent() && enemies[i].type != RobotType.SCOUT){
+                    if (enemies[i].team == rc.getTeam().opponent() && enemies[i].type != RobotType.SCOUT) {
                         rc.broadcastMessageSignal(0, 0, 106); //first 0 indicates hostile, second 0 indicates other player
                         rc.broadcastMessageSignal(enemies[i].location.x, enemies[i].location.y, 106);
                         rc.broadcastMessageSignal(9, 9, 106); //DAVAI
                         rc.broadcastMessageSignal(enemies[i].location.x, enemies[i].location.y, 106);
-                    }
-                    else if(enemies[i].type == RobotType.ZOMBIEDEN){
+                    } else if (enemies[i].type == RobotType.ZOMBIEDEN) {
                         rc.broadcastMessageSignal(0, 0, 106);
                         rc.broadcastMessageSignal(enemies[i].location.x, enemies[i].location.y, 106);
-                    }
-                    else{
+                    } else {
                         rc.broadcastMessageSignal(0, 1, 106);
                         rc.broadcastMessageSignal(enemies[i].location.x, enemies[i].location.y, 106);
                     }
@@ -52,8 +50,7 @@ public class Scout {
                         // Move
                         rc.move(dirToMove);
                         stepCount--;
-                    }
-                    else{
+                    } else {
                         spiralCount++;
                         stepCount = spiralCount * 7 - stepCount;
                         dirToMove = dirToMove.rotateRight();
@@ -68,35 +65,39 @@ public class Scout {
                 }
 
                 RobotInfo[] neutrals = rc.senseNearbyRobots(rc.getLocation(), 53, Team.NEUTRAL);
-                RobotInfo closen = null;
 
-                for (int i = 0; i < neutrals.length; i++)
-                {
-                    if (i == 0){
-                        closen = neutrals[i];
+                if (neutrals.length > 0) {
+                    RobotInfo closen = neutrals[0];
+                    int mind = closen.location.distanceSquaredTo(home);
+                    for (int i = 0; i < neutrals.length; i++) {
+                        int nd = neutrals[i].location.distanceSquaredTo(home);
+                        if (nd < mind) {
+                            closen = neutrals[i];
+                            mind = nd;
+                        }
                     }
-                    else if(neutrals[i].location.distanceSquaredTo(home)<=closen.location.distanceSquaredTo(home)){
-                        closen = neutrals[i];
+                    if (!closen.location.equals(home)) {
+                        rc.broadcastMessageSignal(1, 0, 106);
+                        rc.broadcastMessageSignal(closen.location.x, closen.location.y, 106);
                     }
                 }
-                if(!closen.location.equals(home)){
-                    rc.broadcastMessageSignal(1, 0, 106);
-                    rc.broadcastMessageSignal(closen.location.x, closen.location.y, 106);
-                }
 
-                MapLocation close = home;
                 MapLocation[] parts = rc.sensePartLocations(106);
-                for (int i = 0; i < parts.length; i++) {
-                    if(i ==0){
-                        close = parts[i];
+
+                if (parts.length > 0) {
+                    MapLocation close = parts[0];
+                    int mind = close.distanceSquaredTo(home);
+                    for (int i = 0; i < parts.length; i++) {
+                        int nd = parts[i].distanceSquaredTo(home);
+                        if (nd < mind) {
+                            close = parts[i];
+                            mind = nd;
+                        }
                     }
-                    else if(parts[i].distanceSquaredTo(home)<=close.distanceSquaredTo(home)){
-                        close = parts[i];
+                    if (!close.equals(home)) {
+                        rc.broadcastMessageSignal(1, 0, 106);
+                        rc.broadcastMessageSignal(close.x, close.y, 106);
                     }
-                }
-                if(!close.equals(home)) {
-                    rc.broadcastMessageSignal(1, 0, 106);
-                    rc.broadcastMessageSignal(close.x, close.y, 106);
                 }
                 Clock.yield();
             } catch (Exception e) {
