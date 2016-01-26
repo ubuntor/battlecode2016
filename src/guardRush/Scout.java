50173622fc9fc34411ddc9e7527e3a96360a926d
elita499 <elita499@gmail.com>
2016-01-26 16:06:08 -0500

    SCOUTS NOW KAMIKAZE

--- src/guardRush/Scout.java
+++ src/guardRush/Scout.java
@@ -2,13 +2,31 @@ package guardRush;
 
 import battlecode.common.*;
 
+import java.util.Random;
+
 /**
  * Created by samuel on 1/11/16.
  */
 public class Scout {
     public static void run(RobotController rc) {
+        Random rand = new Random(rc.getID());
+        int distance;
+        int cycle = 0;
         Direction dirToMove;
+        MapLocation originalTarget = rc.getLocation();
+        MapLocation[] targets = rc.getInitialArchonLocations(rc.getTeam().opponent());
+        int targetNum = 0;
+        Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST,
+                Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
         try {
+            distance = 999;
+            for (int i = 0; i < targets.length; i++) {
+                if (targets[i].distanceSquaredTo(rc.getLocation()) < distance) {
+                    targetNum = i;
+                    distance = targets[i].distanceSquaredTo(rc.getLocation());
+                }
+            }
+            originalTarget = targets[targetNum];
             // init stuff
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -16,45 +34,38 @@ public class Scout {
         }
         while (true) {
             try {
-                for(int i = 0; i < 5; i++){
-                    rc.broadcastSignal(10000);
-                }
-                for(int i = 0; i < 20; i++){
-                    rc.broadcastMessageSignal(0,0,10000);
-                }
-                //run the fuck away if you see an enemy
+                //patrol/move
                 if (rc.isCoreReady()) {
-                    RobotInfo[] enemies = rc.senseHostileRobots(rc.getLocation(), 53);
-                    if (enemies.length > 0) {
-                        int enemyDistance = 999;
-                        RobotInfo closestEnemy = null;
-                        for (int i = 0; i < enemies.length; i++) {
-                            if (enemies[i].location.distanceSquaredTo(rc.getLocation()) < enemyDistance && enemies[i].type.canAttack()) {
-                                closestEnemy = enemies[i];
-                                enemyDistance = enemies[i].location.distanceSquaredTo(rc.getLocation());
+                    dirToMove = directions[rand.nextInt(8)];
+                    //move to target
+                    if(cycle == 0) {
+                        if (rc.getLocation().distanceSquaredTo(targets[targetNum]) <= 2) {
+                            targetNum++;
+                            targetNum = targetNum % targets.length;
+                            if (originalTarget == targets[targetNum]) {
+                                cycle = 1;
                             }
+                        } else if (targetNum < targets.length) {
+                            dirToMove = rc.getLocation().directionTo(targets[targetNum]);
                         }
-                        if (closestEnemy != null) {
-                            dirToMove = closestEnemy.location.directionTo(rc.getLocation());
-                            if (rc.canMove(dirToMove)) {
-                                // Move away
-                                rc.move(dirToMove);
-                            } else {
-                                if (rc.canMove(dirToMove.rotateLeft()))
-                                    rc.move(dirToMove.rotateLeft());
-                                else if (rc.canMove(dirToMove.rotateRight()))
-                                    rc.move(dirToMove.rotateRight());
-                                else if (rc.canMove(dirToMove.rotateLeft().rotateLeft()))
-                                    rc.move(dirToMove.rotateLeft().rotateLeft());
-                                else if (rc.canMove(dirToMove.rotateRight().rotateRight()))
-                                    rc.move(dirToMove.rotateRight().rotateRight());
-                                else if (rc.canMove(dirToMove.rotateLeft().rotateLeft().rotateLeft()))
-                                    rc.move(dirToMove.rotateLeft().rotateLeft().rotateLeft());
-                                else if (rc.canMove(dirToMove.rotateRight().rotateRight().rotateRight()))
-                                    rc.move(dirToMove.rotateRight().rotateRight().rotateRight());
-                                // if we still can't move then we're fucked lol
-                            }
-                        }
+                    }
+                    if (rc.canMove(dirToMove)) {
+                        // Move
+                        rc.move(dirToMove);
+                    } else if (rc.canMove(dirToMove.rotateLeft())) {
+                        rc.move(dirToMove.rotateLeft());
+                    } else if (rc.canMove(dirToMove.rotateRight())) {
+                        rc.move(dirToMove.rotateRight());
+                    } else if (rc.senseRubble(rc.getLocation().add(dirToMove)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
+                        // Too much rubble, so I should clear it
+                        rc.clearRubble(dirToMove);
+                        // Check if I can move in this direction
+                    } else if (rc.senseRubble(rc.getLocation().add(dirToMove.rotateLeft())) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
+                        // Too much rubble, so I should clear it
+                        rc.clearRubble(dirToMove.rotateLeft());
+                    } else if (rc.senseRubble(rc.getLocation().add(dirToMove.rotateRight())) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
+                        // Too much rubble, so I should clear it
+                        rc.clearRubble(dirToMove.rotateRight());
                     }
                 }
                 Clock.yield();
